e7df1ce0884c5770b7f78be51d1c6b2238bfbd1b
mahabubr <mahabub.r753@gmail.com>
2025-04-01 22:26:51 +0600

    feat: update intent search endpoint to accept search parameter and integrate FAISS for product vector retrieval

--- app/api/endpoints/intent_search.py
+++ app/api/endpoints/intent_search.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from ..services.intent_search import perform_intent_search
 
 router = APIRouter()
 
 
+class Request(BaseModel):
+    search: str
+
+
 @router.post("/intent-search")
-async def intent_search():
-    result = perform_intent_search()
+async def intent_search(request: Request):
+    search = request.search
+
+    result = perform_intent_search(search)
 
     return result
--- app/api/services/intent_search.py
+++ app/api/services/intent_search.py
@@ -1,4 +1,12 @@
-def perform_intent_search():
-    print("intent search service...")
+import faiss
+import os
+
+index_path = os.getcwd() + "/app/database/products_vector.bin"
+
+index = faiss.read_index(index_path)
+
+
+def perform_intent_search(search):
+    print(index)
 
     return {"message": "Product Get Successful", "data": []}
